4846449f0eda0f3bb4a5c791048e9b4cb7af934e
gabrielglasser <sga91000@gmail.com>
2024-09-30 18:47:57 -0300

    User Controller

--- index.js
+++ index.js
@@ -20,21 +20,3 @@ function addLine(dataUser) {
   document.getElementById("table-users").appendChild(tr);
 }
 
-document
-  .getElementById("form-user-create")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    fields.forEach(function (field, index) {
-      if (field.name == "gender") {
-        if (field.checked) {
-          user[field.name] = field.value;
-        }
-      } else {
-        user[field.name] = field.value;
-      }
-    });
-
-    var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
-    
-    addLine(user);
-  });
