b6abe69667acfd3c982bd53fdd527996cae4797c
gabrielglasser <sga91000@gmail.com>
2024-10-01 00:37:24 -0300

    File Reader

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,45 +1,75 @@
 class UserController {
+  constructor(formId, tableId) {
+    this.formEl = document.getElementById(formId);
+    this.tableEl = document.getElementById(tableId);
 
-    constructor(formId, tableId) {
+    this.onSubmit();
+  }
 
-        this.formEl = document.getElementById(formId);
-        this.tableEl = document.getElementById(tableId);
+  onSubmit() {
+    this.formEl.addEventListener("submit", (event) => {
+      event.preventDefault();
 
-        this.onSubmit();
+      let values = this.getValues();
 
-    }
 
-    onSubmit(){
+      this.getPhoto((content) => {
+        values.photo = content;
+        this.addLine(values);
+      })
 
-        this.formEl.addEventListener("submit", event => {
-            event.preventDefault();
-
-           this.addLine(this.getValues());
-
+      
     });
-};
+  }
 
-    getValues() {
+  getPhoto(callback) {
+    let fileReader = new FileReader();
 
-        let user = {};
+    let elements = [...this.formEl.elements].filter((item) => {
+      if (item.name === "photo") {
+        return item;
+      }
+    });
 
-        [...this.formEl.elements].forEach(function (field, index) {
-            if (field.name == "gender") {
-                if (field.checked) {
-                    user[field.name] = field.value;
-                }
-            } else {
-                user[field.name] = field.value;
-            }
-        });
+    let file = elements[0].files[0];
 
-        return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+    fileReader.onload = () => {
+
+        callback(fileReader.result);
     };
 
-     addLine(dataUser) {
-        this.tableEl.innerHTML = `<tr>
+    fileReader.readAsDataURL(file);
+  }
+
+  getValues() {
+    let user = {};
+
+    [...this.formEl.elements].forEach(function (field, index) {
+      if (field.name == "gender") {
+        if (field.checked) {
+          user[field.name] = field.value;
+        }
+      } else {
+        user[field.name] = field.value;
+      }
+    });
+
+    return new User(
+      user.name,
+      user.gender,
+      user.birth,
+      user.country,
+      user.email,
+      user.password,
+      user.photo,
+      user.admin
+    );
+  }
+
+  addLine(dataUser) {
+    this.tableEl.innerHTML = `<tr>
                           <td>
-                            <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+                            <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
                           </td>
                           <td>${dataUser.name}</td>
                           <td>${dataUser.email}</td>
@@ -50,10 +80,7 @@ class UserController {
                             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                           </td>
                         </tr>`;
-      
-        document.getElementById("table-users").appendChild(tr);
-      }
-      
-      
 
-};
+    document.getElementById("table-users").appendChild(tr);
+  }
+}
