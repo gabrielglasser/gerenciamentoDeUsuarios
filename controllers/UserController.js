24c4f69cd6c4a42d57dc83a72e6cbaa51703c058
gabrielglasser <sga91000@gmail.com>
2024-10-05 00:46:11 -0300

    Programando o Evento no Botao de Editar e Cancelar

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,17 @@ class UserController {
     this.tableEl = document.getElementById(tableId);
 
     this.onSubmit();
+    this.onEdit();
+  }
+
+  onEdit() {
+
+    document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+
+      this.showPanelCreate();
+
+    });
+
   }
 
   onSubmit() {
@@ -112,13 +123,33 @@ class UserController {
                     <td>${(dataUser.admin) ? 'Sim' : 'Nao'}</td>
                     <td>${Utils.dateFormat(dataUser.register)}</td>
                     <td>
-                      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                      <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                       <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                     </td>`;
+
+    tr.querySelector('.btn-edit').addEventListener("click", e => {
+
+      JSON.parse(tr.dataset.user);
+      this.showPanelUpdate();
+
+    })
 
     this.tableEl.appendChild(tr);
 
     this.updateCount();
+  }
+
+  showPanelCreate() {
+
+    document.querySelector('#box-user-create').style.display = "block";
+      document.querySelector('#box-user-update').style.display = "none";
+
+  }
+  showPanelUpdate() {
+
+    document.querySelector('#box-user-create').style.display = "none";
+    document.querySelector('#box-user-update').style.display = "block";
+
   }
 
   updateCount() {
@@ -132,9 +163,10 @@ class UserController {
 
       let user = JSON.parse(tr.dataset.user);
 
-      
-      
-      if (user._admin) numberAdmin++;    });
+
+
+      if (user._admin) numberAdmin++;
+    });
 
     document.querySelector("#number-users").innerHTML = numberUsers;
     document.querySelector("#number-users-admin").innerHTML = numberAdmin;
