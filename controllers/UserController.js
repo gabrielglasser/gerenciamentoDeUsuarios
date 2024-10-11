1f5843e7d3bbca40b1315b881334d7b533281adf
gabrielglasser <sga91000@gmail.com>
2024-10-11 02:18:32 -0300

    excluindo dados com localStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -160,20 +160,8 @@ class UserController {
     );
   }
 
-  getUsersStorage() {
-    let users = [];
-
-    if (localStorage.getItem("users")) {
-
-      users = JSON.parse(localStorage.getItem("users"));
-
-    }
-    return users;
-
-  }
-
   selectAll() {
-    let users = this.getUsersStorage();
+    let users = User.getUsersStorage();
 
     users.forEach(dataUser => {
 
@@ -226,6 +214,11 @@ class UserController {
     tr.querySelector(".btn-delete").addEventListener("click", (e) => {
 
       if (confirm("Deseja realmente excluir?")) {
+
+        let user = new User();
+
+        user.loadFromJSON(JSON.parse(tr.dataset.user));
+        user.remove();
 
         tr.remove();
 
--- models/User.js
+++ models/User.js
@@ -88,11 +88,15 @@ class User {
 
     getNewID() {
 
-        if (!window.id) window.id = 0;
+        let usersID = parseInt(localStorage.getItem("usersID"));
 
-        id++;
+        if (!usersID > 0)  usersID = 0;
 
-        return id;
+        usersID++;
+
+        localStorage.setItem("usersID", usersID)
+
+        return usersID;
 
     }
 
@@ -104,9 +108,9 @@ class User {
 
             users.map(u => {
 
-                if (u._id === this.id) {
+                if (u._id == this.id) {
 
-                    u = this;
+                    Object.assign(u, this);
 
                 }
 
@@ -121,6 +125,24 @@ class User {
             users.push(this);
         }
         localStorage.setItem("users", JSON.stringify(users));
+    }
+
+    remove() {
+
+        let users = User.getUsersStorage();
+
+        users.forEach((userData, index) => {
+
+            if (this._id == userData._id) {
+
+                users.splice(index, 1);
+
+            }
+
+        });
+
+        localStorage.setItem("users", JSON.stringify(users));
+
     }
 
 
